f188234944a3ca7f5689a5596c43403bf6d098d1
Solwet <rmuhtarov54@gmail.com>
2024-10-12 12:31:31 +0500

    Готовое решение

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -1,12 +1,20 @@
 import math
 
-a = float(input("Введите начало границы поиска: "))
-b = float(input("Введите конец границы поиска: "))
+def tryfloat(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+           
+            print("Ошибка: Введите корректное число.")
 
-start = math.ceil(min(a, b))  
-end = math.floor(max(a, b))
+a = tryfloat("Введите начало границы поиска: ")
+b = tryfloat("Введите конец границы поиска: ")
+
+start = math.floor(min(a, b))  
+end = math.ceil(max(a, b))
 result=[]
-for i in range(start,end):
+for i in range(start+1,end):
     result.append(i**2)
 
 print(result)
