03970a2c913cff3ba1fad8f24c2a7ee1f8422ef0
Solwet <rmuhtarov54@gmail.com>
2024-10-12 12:19:02 +0500

    Границы

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -1,3 +1,11 @@
-a = float(input("Введите границы поиска"))
-b = float(input("Введите границы поиска"))
-print(a,b)
+import math
+
+a = float(input("Введите начало границы поиска: "))
+b = float(input("Введите конец границы поиска: "))
+
+start = math.ceil(min(a, b))  
+end = math.floor(max(a, b))
+
+print(start,end)
+
+
