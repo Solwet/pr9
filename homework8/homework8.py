d96c871a1112b1867b13a5b176a35b437a9a591f
Solwet <rmuhtarov54@gmail.com>
2024-10-12 13:50:16 +0500

    Двумерный массив

--- homework8/homework8.py
+++ homework8/homework8.py
@@ -14,13 +14,25 @@ def intinput(prompt):
 def create_bilet_list(rows, cols):
     bilet_list = []
     for _ in range(rows):
+<<<<<<< HEAD
+=======
+        # Создание подсписка с случайными числами
+>>>>>>> 842aeff (Двумерный массив)
         sublist = [random.randint(1, 100) for _ in range(cols)]
         bilet_list.append(sublist)
     return bilet_list
 
+<<<<<<< HEAD
 rows = intinput("Введите количество строк: ")
 cols = intinput("Введите количество столбцов: ")
 
+=======
+# Ввод количества строк и столбцов
+rows = intinput("Введите количество строк: ")
+cols = intinput("Введите количество столбцов: ")
+
+# Создание и вывод двумерного списка
+>>>>>>> 842aeff (Двумерный массив)
 bilet = create_bilet_list(rows, cols)
 print("Созданный список списков:")
 for sublist in bilet:
