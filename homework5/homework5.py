11f189f61792128697536b5ce22b78588dca8607
Solwet <rmuhtarov54@gmail.com>
2024-10-12 13:18:50 +0500

    Решение

--- homework5/homework5.py
+++ homework5/homework5.py
@@ -1,8 +1,29 @@
 import random
 numbers=[]
+result=[]
 
-count=int(input("Введите количество чисел в списке:"))
+def intinput(prompt):
+    while True:
+        try:
+            Value = int(input(prompt))
+            if Value > 0:
+                return Value
+            else:
+                print("Ошибка: Введите положительное целое число.")
+        except ValueError:
+            print("Ошибка: Введите корректное целое число.")
+
+count=intinput("Введите количество чисел в списке:")
 for i in range(count):
     numbers.append(random.randint(-100,100))
 
-print(numbers)
+for i in range(count-1):
+    if numbers[i] < numbers[i+1]:
+        result.append(numbers[i+1])
+
+if len(result) == 0:
+    result.append("Таких элементов не найдено")
+print("Изначальный список",numbers,"Элементы которые больше предыдущего элемента",result)
+
+
+
